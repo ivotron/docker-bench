17f560eab15d21350e4535f90eff8facb5b36d62
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-10-22 22:15:20 -0700

    lulesh: mods to reflect changes to ivotron/openmpi

--- lulesh/entrypoint.sh
+++ lulesh/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ -n "$RANK0" ] ; then
-  mpirun_docker lulesh2.0 $@ &> lulesh_output
+export SKIP_MPISTOP=1
 
+mpirun_docker lulesh2.0 $@ &> lulesh_output
+
+if [ -n "$RANK0" ] ; then
   result=`cat lulesh_output | grep 'Elapsed time' | awk '{print $4}'`
   echo "[{"
   echo "\"name\": \"lulesh\", "
@@ -11,9 +13,6 @@ if [ -n "$RANK0" ] ; then
   echo "\"lower_is_better\": true, "
   echo "\"result\": \"$result\" "
   echo "}]"
-
-  mpistop
-
-else
-  /root/entrypoint.sh
 fi
+
+mpistop
