4040a9db2f65b033f6ad96cd3999587683045b82
Ivo Jimenez <ivo.jimenez@gmail.com>
2018-05-06 00:10:06 -0700

    Moves output for pybench and silt to /results folder

--- pybench/entrypoint.sh
+++ pybench/entrypoint.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
-set -e
+set -ex
 
 if [ -z "$ITERATIONS" ] ; then
   ITERATIONS=3
 fi
 
-/root/pybench-2009-08-14/pybench.py -n 3 -C 0 > results
+/root/pybench-2009-08-14/pybench.py -n 3 -C 0 > output.txt
 
-echo "["
-cat results | \
-  grep ".*:.*ms.*ms.*us.*ms" | \
-  awk '{print $1 $3}' | \
-  sed 's/\(.*\):\(.*\)ms/{"name": "pybench_\1", "result": \2, "lower_is_better": true, "units": "microseconds"},/' | \
-  sed '$ s/.$//'
-echo "]"
+if [ -d /results ]; then
+  mv output.txt /results/
+else
+  cat output.txt
+fi
--- silt/entrypoint.sh
+++ silt/entrypoint.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
+set -ex
 cd /silt/test/fawnds
-./benchCuckoo > output
-insertrate=`sed -n 's/cuckoo: \(.*\) inserts\/sec/\1/p' output`
-hitrate=`sed -n 's/cuckoo: \(.*\) lookups\/sec (hit)/\1/p' output`
-missrate=`sed -n 's/cuckoo: \(.*\) lookups\/sec (miss)/\1/p' output`
-
-cat > results.json << EOL
-[
-  {"name": "cuckoo-insert-rate", "result": $insertrate},
-  {"name": "cuckoo-hit-rate", "result": $hitrate},
-  {"name": "cuckoo-miss-rate", "result": $missrate}
-]
-EOL
-
+./benchCuckoo > output.txt
 if [ -d /results ]; then
-  mv results.json /results/
+  mv output.txt /results/
+else
+  cat output.txt
 fi
